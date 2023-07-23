c14035f014a31bc5bc601326b9ab776048af52ba
Satimov727 <125095778+Satimov727@users.noreply.github.com>
2023-07-23 23:15:40 +0300

    Update App.tsx

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,24 +1,32 @@
-import React from 'react'
+import {useState} from 'react'
 import { Bar } from './components/Bar'
 import { Product } from './components/Product'
 import { Input } from './components/Input'
 
 function App() {
+  const [products, setProducts] = useState([
+    { text: 'Wake up' },
+    { text: 'Have breakfast' },
+    { text: 'Exercise' },
+    { text: 'Learn japanese(N?)' },
+    { text: 'Play games' },
+    { text: 'Sleep' },
+  ])
 
   return (
-    <>
+    <main>
       
       <Bar />
 
       <div className='space-y-10'>
 
-        <Input />
+        <Input addTask={(text) => setProducts([...products, {text}])}/>
 
-        <Product />
+        <Product products={products} />
         
       </div >
       
-    </>
+    </main>
   )
 
 }
